1799c7b6c3004edaf91b6b34f2cc7cac4770a4c7
juankmiloh <juankmiloardila@gmail.com>
2020-03-15 00:07:22 -0500

    Se crea funcion para actualizar mapa sin recargar

--- src/app/first-page/first-page.component.ts
+++ src/app/first-page/first-page.component.ts
@@ -8,11 +8,59 @@ import { loadModules } from 'esri-loader';
 })
 export class FirstPageComponent implements OnInit, OnDestroy {
 
+  constructor() {}
+
+  zoom = 0;
+  basemapToggle: any;
+
   // The <div> where we will place the map
   @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
   view: any;
 
-  constructor() {}
+  async updateMap(value: number) {
+    this.zoom = value;
+    this.view.zoom = this.zoom;
+    this.view.center = [-74.5, 6.2];
+    this.view.map.basemap = 'dark-gray-vector';
+    console.log('Propiedades Mapa: ', this.view.map.layers.items[0].url);
+    console.log('Propiedades basemap: ', this.view.map);
+    const [CSVLayer, BasemapToggle] = await loadModules(['esri/layers/CSVLayer', 'esri/widgets/BasemapToggle']);
+    const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv';
+    const template = {
+      title: '{place}',
+      content: 'Magnitude {mag} {type} hit {place} on {time}.'
+    };
+    const renderer = {
+      type: 'heatmap',
+      // field: 'numero_pqrs',
+      colorStops: [
+        { color: 'rgba(63, 40, 102, 0)', ratio: 0 }, // rango de 0 a 1
+        { color: '#6300df', ratio: 0.083 },          // Azul claro
+        { color: '#002dfe', ratio: 0.100 },          // Azul
+        { color: '#00ff2c', ratio: 0.166 },          // Verde Clarito
+        { color: '#a1ff00', ratio: 0.249 },          // Verde
+        { color: '#e5ff00', ratio: 0.332 },          // Amarillo claro
+        { color: '#fef700', ratio: 0.415 },          // Amarillo
+        { color: '#ffc700', ratio: 0.498 },          // Amarillo oscuro
+        { color: '#fea701', ratio: 0.581 },          // Naranja claro
+        { color: '#ff6400', ratio: 0.664 },          // Naranja
+        { color: '#ff3000', ratio: 1 }               // Rojo
+      ],
+      maxPixelIntensity: 2000,
+      minPixelIntensity: 50
+    };
+    const layer = new CSVLayer({
+      url,
+      title: 'Agregar Título',
+      copyright: 'DESARROLLADO POR JUAN CAMILO HERRERA - CIAD SSPD',
+      popupTemplate: template,
+      renderer
+    });
+    this.view.map.layers = layer; // Se agrega un nuevo layer CSV al mapa
+    this.view.ui.remove([this.basemapToggle]);
+    this.basemapToggle = new BasemapToggle({view: this.view, nextBasemap: 'streets-navigation-vector'});
+    this.view.ui.add(this.basemapToggle, 'top-right'); // Muestra las opciones del mapa base
+  }
 
   async initializeMap() {
     try {
@@ -20,7 +68,8 @@ export class FirstPageComponent implements OnInit, OnDestroy {
       // tslint:disable-next-line: max-line-length
       const [Track, Map, MapView, CSVLayer, Search, Legend, BasemapToggle, watchUtils] = await loadModules(['esri/widgets/Track', 'esri/Map', 'esri/views/MapView', 'esri/layers/CSVLayer', 'esri/widgets/Search', 'esri/widgets/Legend', 'esri/widgets/BasemapToggle', 'esri/core/watchUtils']);
 
-      const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv';
+      // const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.csv';
+      const url = '';
 
       // Paste the url into a browser's address bar to download and view the attributes
       // in the CSV file. These attributes include:
@@ -69,7 +118,7 @@ export class FirstPageComponent implements OnInit, OnDestroy {
 
       // Configure the Map
       const mapProperties = {
-        basemap: 'dark-gray-vector',
+        basemap: 'streets-navigation-vector',
         layers: [layer]
       };
 
@@ -78,8 +127,8 @@ export class FirstPageComponent implements OnInit, OnDestroy {
       // Initialize the MapView
       const mapViewProperties = {
         container: this.mapViewEl.nativeElement,
-        center: [-74.5, 6.2], // [horizontal (long), vertical (lat)]
-        zoom: 5,
+        center: [-74.5, 4.5], // [horizontal (long), vertical (lat)]
+        zoom: 4,
         map
       };
 
@@ -89,24 +138,26 @@ export class FirstPageComponent implements OnInit, OnDestroy {
       watchUtils.whenTrue(this.view, 'updating', (evt: any) => {
         // showLoad();
         console.log('showLoad', evt);
+        console.log('showCoordenadas', this.view.center);
       });
 
       // Hide the loading indicator when the view stops updating
       watchUtils.whenFalse(this.view, 'updating', (evt: any) => {
         console.log('closeLoad', evt);
+        console.log('closeCoordenadas', this.view.center); // guardar estas coordenadas en localStorage para cuando se haga una busqueda se actualice el mapa en las mismas coordenadas
       });
 
       const legend = new Legend({view: this.view});
       const search = new Search({view: this.view});
-      const basemapToggle = new BasemapToggle({view: this.view, nextBasemap: 'streets-navigation-vector'});
+      this.basemapToggle = new BasemapToggle({view: this.view, nextBasemap: 'dark-gray-vector'});
       const track = new Track({view: this.view});
 
       this.view.ui.add(legend, 'bottom-left'); // Muestra las convenciones del mapa
       this.view.ui.add(search, {position: 'top-right'}); // Muestra el input de busqueda
-      this.view.ui.move(['zoom', basemapToggle], 'top-left'); // Mover los botones de zoom a la izquierda
+      this.view.ui.move(['zoom', this.basemapToggle], 'top-left'); // Mover los botones de zoom a la izquierda
       this.view.ui.add(track, 'top-left'); // Muestra el boton de MyLocation
       // this.view.ui.remove([basemapToggle, 'zoom']);
-      this.view.ui.add(basemapToggle, 'top-right'); // Muestra las opciones del mapa base
+      this.view.ui.add(this.basemapToggle, 'top-right'); // Muestra las opciones del mapa base
 
       return this.view;
 
